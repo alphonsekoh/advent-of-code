3fae315ff9fda7da56126238bc273b22f73e665d
Koh Huan Yin (David) <alphonsekoh@gmail.com>
2022-12-04 01:18:31 +0800

    :fire: feat: day2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -66,5 +66,51 @@ def find_score() -> int:
 				score += (3+3)
 	return score
 
+"""
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+"""
+def find_score_2() -> int:
+	"""
+	Algorithm to find the score of the game.
+	Opponent chooses A (Rock), B (Paper), or C (Scissors). 
+	You choose X(lose), Y(draw), or Z(win).
+	rock = 1, paper = 2, scissor = 3, Win + 6, Draw + 3, Lose + 0
+	"""
+	rounds = read_input()
+	score = 0
+	for i in range(len(rounds)):
+		if rounds[i][0] == 'A':
+			if rounds[i][2] == 'X':
+				score += (3+0)
+			elif rounds[i][2] == 'Y':
+				score += (1+3)
+			else:
+				score += (2+6)
+		elif rounds[i][0] == 'B':
+			if rounds[i][2] == 'X':
+				score += (1+0)
+			elif rounds[i][2] == 'Y':
+				score += (2+3)
+			else:
+				score += (3+6)
+		else:
+			if rounds[i][2] == 'X':
+				score += (2+0)
+			elif rounds[i][2] == 'Y':
+				score += (3+3)
+			else:
+				score += (1+6)
+	return score
 if __name__ == "__main__":
-	print(find_score())
+	print(f"Part 1 Scores: {find_score()}") # Part 1
+	print(f"Part 2 Scores: {find_score_2()}") # Part 2 
