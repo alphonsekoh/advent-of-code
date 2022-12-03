fb5d3d9272a504a616cccd53370882c002be09da
Koh Huan Yin (David) <alphonsekoh@gmail.com>
2022-12-04 03:09:55 +0800

    :sparkles: feat: day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -32,6 +32,13 @@ In the above example, the priority of the item type that appears in both compart
 """
 import os 
 
+TEST_LIST = ["vJrwpWtwJgWrhcsFMMfFFhFp",
+			"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+			"PmmdzqPrVvPwwTWBwg",
+			"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+			"ttgJtRGJQctTZtZT",
+			"CrZsJsPPZsGzwwsLwLmpwMDw"]
+
 def read_input() -> list:
 	"""
 	Read the input file and return a list of characters.
@@ -56,20 +63,61 @@ def rucksack_sort() -> list:
 				break
 	return rucksack_list
 
-def sum_priorities() -> int:
+def sum_priorities(rlist) -> int:
 	"""
 	Sum the priorities of the characters that appear in both compartments of each rucksack.
 	"""
-	rucksack_list = rucksack_sort()
+	rucksack_list = rlist
 	priorities = []
 	for i in rucksack_list:
 		if i.islower():
 			priorities.append(ord(i) - ord("a") + 1)
 		else:
 			priorities.append(ord(i) - ord("A") + 27)
-	print(priorities)
 	return sum(priorities)
 
+"""
+--- Part Two ---
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+"""
+
+def rucksack_sort2() -> list:
+	"""
+	Find the character that appear in all three compartments of each rucksack and appends them to a list
+	"""
+	apu = read_input()
+	rucksack_list = []
+	for i in range(0, len(apu), 3):
+		first_compartment = apu[i]
+		second_compartment = apu[i+1]
+		third_compartment = apu[i+2]
+		for j in first_compartment:
+			if j in second_compartment and j in third_compartment:
+				rucksack_list.append(j)
+				break
+	return rucksack_list
 if __name__ == '__main__':
-	print(rucksack_sort())
-	print(f"Part 1 Priority sum: {sum_priorities()}")
+	print(f"Part 1 Priority sum: {sum_priorities(rucksack_sort())}") # Part 1
+	print(f"Part 2 Priority sum: {sum_priorities(rucksack_sort2())}") # Part 2
