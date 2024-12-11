23a14c12f52ef68876c578872bbef6a3d900d908
Koh Huan Yin (David) <alphonsekoh@gmail.com>
2024-12-11 23:57:27 +0800

    AoC2024-day1 part2

--- y2024/aoc2024/src/day01/index.ts
+++ y2024/aoc2024/src/day01/index.ts
@@ -18,14 +18,30 @@ const part1 = (rawInput: string) => {
     let min = Math.min(+evenIdxArray[i], +OddIdxArray[i]);
     sum += max - min;
   }
-  console.log(sum);
   return sum;
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  let similarityScore = 0;
+  // Parse input into 2 arrays of numbers
+  // 1. Put all numbers in one consolidated array, split them by 3 spaces and then split by newline
+  // 2. Iterate the consolidated array and remove the even indexes and put them in a separate array
+  const allNumbers = input.split("   ").flatMap((line) => line.split("\n"));
+  const evenIdxArray = allNumbers.filter((_, i) => i % 2 === 0);
+  const OddIdxArray = allNumbers.filter((_, i) => i % 2 !== 0);
 
-  return;
+  // Compare evenIdxArray and find the number of instances in OddIdxArray 
+  for (let i = 0; i < evenIdxArray.length; i++) {
+    let instance = 0;
+    for (let j = 0; j < OddIdxArray.length; j++) {
+      if (evenIdxArray[i] === OddIdxArray[j]) {
+        instance++;
+      }
+    }
+    similarityScore += (instance * +evenIdxArray[i]);
+  }
+  return similarityScore;
 };
 
 run({
